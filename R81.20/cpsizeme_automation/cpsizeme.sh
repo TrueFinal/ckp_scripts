eebe32a7c06a097751db7c01ea5f6353acac19c1
murillo <murillovipcampeao@gmail.com>
2025-01-28 18:26:12 -0300

    Modificação no arquivo cpsizeme.sh, para teste setado em 60 segundos. Modificação do arquivo README.md

--- R81.20/cpsizeme_automation/cpsizeme.sh
+++ R81.20/cpsizeme_automation/cpsizeme.sh
@@ -1,1 +1,37 @@
-#Script para automatização da execução do comando "cpsizeme" em ambientes Checkpoint na versão R81.20
+#!/bin/bash
+
+echo "Script para automatização da execução do comando "cpsizeme run" em ambientes Checkpoint na versão R81.20"
+echo "Por padrão, este script executa um cpsizeme de 24 horas"
+echo "Para mais informações ou acesso ao código fonte, visite: https://github.com/TrueFinal/ckp_scripts"
+
+# Diretório padrão de armazenagem dos arquivos gerados
+OUTPUT_DIR="/var/log/cpsizeme_reports"
+mkdir -p "${OUTPUT_DIR}"
+
+# Nome do arquivo com timestamp
+REPORT_NAME="cpsizeme-report-$(date +%F).tar"
+OUTPUT_PATH="${OUTPUT_DIR}/${REPORT_NAME}"
+
+# Define a duração da execução do comando para 24 horas (tempo em minutos) 
+DURATION=60
+
+echo "=============================================="
+echo " Iniciando CPSIZEME com duração de 24 horas..."
+echo " O arquivo gerado será salvo em: ${OUTPUT_PATH}"
+echo "=============================================="
+
+# Executa o comando cpsizeme por 24 horas (tempo em minutos)
+timeout "${DURATION}" cpsizeme run -o "${OUTPUT_PATH}"
+
+# Verifica se o comando foi concluído com sucesso ou gera menssagem de erro
+if [ $? -eq 0 ]; then
+    echo "=============================================="
+    echo " CPSIZEME concluído com sucesso!"
+    echo " Arquivo gerado em: ${OUTPUT_PATH}"
+    echo "=============================================="
+else
+    echo "=============================================="
+    echo " Houve um problema durante a execução do CPSIZEME."
+    echo " Verifique logs e parâmetros."
+    echo "=============================================="
+fi
--- README.md
+++ README.md
@@ -8,6 +8,11 @@ Este repositório contém uma coleção de scripts úteis para automação, admi
    - **Descrição**: Automatização da criação de template e configurações iniciais do First Time Wizard na versão R81.20 *WIP*.
    - **Uso**: Criação de template de para configuração do First Time Wizard na versão R81.20 *WIP*.
 
+### 1. **CPSIZEME 24H run**
+   - **Descrição**: Automatiza a execução do comando "cpsizeme" durante 24 horas na versão R81.20.
+   - **Uso**: Geração de um arquivo para dimensionamento da utilização de recursos da apliance onde for executado. Usado em assessment's juntamente com "doctorlog".
+   - **Disclaimer**: PARA TESTE APENAS! CONFIGURADO ATUALMENTE PARA EXECUTAR O CPSIZEME DE 1 MINUTO - 60 SEGUNDOS
+
 ## Como Usar
 
 1. Clone este repositório:
@@ -32,4 +37,4 @@ Contribuições são bem-vindas! Sinta-se à vontade para abrir issues e enviar 
 
 - Teste sempre os scripts em um ambiente de homologação antes de utilizá-los em produção.
 - Este repositório é fornecido "como está" e não nos responsabilizamos por quaisquer problemas decorrentes do uso.
-- Considerar que esté repositório até em WIP (Work In Progress), os scripts disponíveis não estão finalizados.
+- Considerar que esté repositório está em WIP (Work In Progress), os scripts disponíveis não estão finalizados.
